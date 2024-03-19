22dfdfd2a7ab1c267c92d0248b2d30f2da36ada9
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-03-19 20:43:48 +0700

    handle input

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -3,14 +3,31 @@ import "./index.css"
 
 const Homepage = () => {
     const [state, setState] = useState(0)
+    const [data, setData] = useState({
+        nama: "Andi",
+        age: 20
+    })
 
     const handleClick = () => {
         setState(state + 1)
     }
 
     const handleKurang = () => {
-        setState(state - 1)
+        if(state > 0) setState(state - 1)
     }
+
+    const handleInput = (e) => {
+        const value = e.target?.value
+        const name = e.target?.name
+
+        setData({
+            ...data, // spread operator
+            [name]: value
+            // nama: value
+        })
+    }
+
+    console.log(data)
 
     return (
         <div className="homepage">
@@ -18,6 +35,14 @@ const Homepage = () => {
             <h2>{state}</h2>
             <button onClick={handleClick}>Click me</button>
             <button onClick={handleKurang}>Kurang</button>
+
+            <div>
+                <div>Email</div>
+                <input name="nama" onChange={(e) => handleInput(e)} />
+
+                <div>age</div>
+                <input type="number" name="age" onChange={(e) => handleInput(e)} />
+            </div>
         </div>
     );
 }
