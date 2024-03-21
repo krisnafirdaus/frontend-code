609671c2f6dc629c725a484d77a9e1e32276fd8c
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-03-21 19:37:18 +0700

    add map reactjs

--- src/pages/detail/detail.jsx
+++ src/pages/detail/detail.jsx
@@ -2,20 +2,28 @@ import {useState, useEffect} from 'react'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import { InputAdornment, Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { InputAdornment, Box, Paper } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import CardComponent from "../../components/CardComponent"
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Detail = () => {
     const [state, setState] = useState(false)
-    const [data, setData] = useState({})
+    const [data, setData] = useState([])
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(json => setData(json))
     }, []) // dependency
-
-    console.log(data)
 
     return (
         <div>
@@ -30,14 +38,23 @@ const Detail = () => {
             }} />
 
            <Box
-            display={'flex'}
-            justifyContent={'space-between'}
             mt={10}
-            sx={{ backgroundColor: 'red' }}
            >
-                <CardComponent />
-                <CardComponent />
-                <CardComponent />
+						 <Grid container spacing={0}>
+								{data.map((item, index) => {
+									console.log(index)
+									return (
+										<Grid key={index} xs={4}>
+											<Item>
+												<CardComponent
+													title={item.title}
+													body={item.body}
+												/>
+											</Item>
+										</Grid>
+									)
+								})}
+								</Grid>
            </Box>
         </div>
     )
