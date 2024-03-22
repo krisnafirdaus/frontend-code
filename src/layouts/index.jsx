1b89ec5024447ec8d8c5c66a9c18af1cb5145020
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-03-22 20:49:37 +0700

    change code navbar

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,23 +3,34 @@ import { Outlet, Link, useLocation} from 'react-router-dom'
 const Layout = () => {
     const {pathname} = useLocation()
 
-    console.log(pathname)
+    const navData = [
+        {
+            path: '/',
+            name: 'Home'
+        },
+        {
+            path: '/email-confirmation',
+            name: 'Email'
+        },
+        {
+            path: '/detail',
+            name: 'Detail'
+        },
+        {
+            path: '/forgot-password',
+            name: 'Forgot'
+        }
+    ]
+
     return (
         <div>
            <nav>
                 <ul>
-                <li>
-                    <Link to="/" style={{ color: pathname === '/' && 'red'}}>Home</Link>
-                </li>
-                <li>
-                    <Link to="/email-confirmation" style={{ color: pathname === '/email-confirmation' && 'red'}}>Email</Link>
-                </li>
-                <li>
-                    <Link to="/detail" style={{ color: pathname === '/detail' && 'red'}}>Detail</Link>
-                </li>
-                <li>
-                    <Link to="/forgot-password" style={{ color: pathname === '/forgot-password' && 'red'}}>Forgot</Link>
-                </li>
+                    {navData.map((item, index) => (
+                        <li key={index}>
+                            <Link to={item.path} style={{ color: pathname === item.path && 'red' }}>{item.name}</Link>
+                        </li>
+                    ))}
                 </ul>
             </nav>
             <Outlet />
